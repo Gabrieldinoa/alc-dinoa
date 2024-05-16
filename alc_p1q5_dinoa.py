003d4936e71e0186b83e0f8c8b959e81a6b6d3fd
Gabrieldinoa <166359363+Gabrieldinoa@users.noreply.github.com>
2024-05-16 18:44:19 -0300

    Update alc_p1q5_dinoa.py

--- alc_p1q5_dinoa.py
+++ alc_p1q5_dinoa.py
@@ -2,59 +2,56 @@ import numpy as np
 import math as m
 
 matriz=np.array([[1,2,3,2],[2,3,4,7],[1,3,2,2],[3,4,6,10]])
+tam=len(matriz)
+print(tam)
 print(matriz)
 
-#identidade=np.eye(2)
-#print(identidade)
+identidade=np.eye(tam)
+print(identidade)
 
 #zerar a primeira coluna
-for n in range(1,4):
-    print(n)
-    matriz[n]=matriz[0][0]*matriz[n]-matriz[n][0]*matriz[0]
+for k in range(0,tam-1):
+    print(k)
+    for n in range(k+1,tam):
+        print(n)
+        matriz[n]=matriz[k][k]*matriz[n]-matriz[n][k]*matriz[k]
+        identidade[n]=identidade[k][k]*identidade[n]-identidade[n][k]*identidade[k]
 
-print(matriz)
-
-#zerar a segunda coluna
-
-for n in range(2,4):
-    print(n)
-    matriz[n]=matriz[1][1]*matriz[n]-matriz[n][1]*matriz[1]
-
-print(matriz)
-
-#zerar a terceira coluna
-
-for n in range(3,4):
-    print(n)
-    matriz[n]=matriz[2][2]*matriz[n]-matriz[n][2]*matriz[2]
-
+    
 print(matriz)
 
 #zerar a parte superior
 
 #zerar a segunda coluna
-for n in range(0,1):
-    print(n)
-    matriz[n]=matriz[1][1]*matriz[n]-matriz[n][1]*matriz[1]
+for t in range(1,tam):
+    print(t)
+    for n in range(0,t):
+        print(t)
+        matriz[n]=matriz[t][t]*matriz[n]-matriz[n][t]*matriz[t]
+        identidade[n]=identidade[t][t]*identidade[n]-identidade[n][t]*identidade[t]
 
 print(matriz)
 
+# for n in range(0,1):
+#     print(n)
+#     matriz[n]=matriz[1][1]*matriz[n]-matriz[n][1]*matriz[1]
 
+# print(matriz)
 
-#zerar a terceira coluna
+# #zerar a terceira coluna
 
-for n in range(0,2):
-    print(n)
-    matriz[n]=matriz[2][2]*matriz[n]-matriz[n][2]*matriz[2]
+# for n in range(0,2):
+#     print(n)
+#     matriz[n]=matriz[2][2]*matriz[n]-matriz[n][2]*matriz[2]
 
-print(matriz)
+# print(matriz)
 
+# #zerar a quarta coluna
 
-#zerar a quarta coluna
+# for n in range(0,3):
+#     print(n)
+#     matriz[n]=matriz[3][3]*matriz[n]-matriz[n][3]*matriz[3]
 
-for n in range(0,3):
-    print(n)
-    matriz[n]=matriz[3][3]*matriz[n]-matriz[n][3]*matriz[3]
+# print(matriz)
 
-print(matriz)
-
+print(identidade[1])
