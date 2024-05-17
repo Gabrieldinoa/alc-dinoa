717b3b6e4037cf8605a74b81fa6a45fa6c368b95
Gabrieldinoa <166359363+Gabrieldinoa@users.noreply.github.com>
2024-05-16 23:38:40 -0300

    Update alc_p1q5_dinoa.py

--- alc_p1q5_dinoa.py
+++ alc_p1q5_dinoa.py
@@ -4,11 +4,13 @@ import math as m
 def inversa_eliminacao(matriz):
 
 
-    
+    #Calcula o tamanho da matriz
     tam=len(matriz)
-    #print(tam)
+    #Plota a matriz de entrada
     print('A matriz inicial é:')
     print(matriz)
+
+    #Verifica a existência de zeros na diagonal principal
 
     avaliador=1
     for cont in range(0,tam):
@@ -16,16 +18,20 @@ def inversa_eliminacao(matriz):
     if avaliador==0:
         print('Zero na diagonal principal: Sugere-se o uso de uma função alternativa para o cálculo da inversa')
     else:
-        matriz_inversa=np.linalg.inv(matriz)
+        #Cálculo da inversa pela função inv do numpy, para comparação
+        
+
+        #Criação da matriz aumentada
         identidade=np.eye(tam)
-        
-        
         aumentada=np.concatenate((np.transpose(matriz),identidade))
         aT=np.transpose(aumentada)
         matriz=aT
         print('A matriz aumentada é:')
         print(matriz)
         
+        #Laços das operações de diagonalização na matriz aumentada
+
+        #Zerando os elementos abaixo da diagonal principal da matriz original
         for k in range(0,tam-1):
             
             if matriz[k][k]==0:
@@ -41,7 +47,7 @@ def inversa_eliminacao(matriz):
                 matriz[n]=matriz[k][k]*matriz[n]-matriz[n][k]*matriz[k]
                 
                 
-            
+        #Zerando os elementos acima da diagonal principal da matriz original    
         for t in range(1,tam):
             
             for n in range(0,t):
@@ -50,19 +56,26 @@ def inversa_eliminacao(matriz):
                 
             
         matriz=(1/matriz[0][0])*matriz
+
+        #Plotando a matriz aumentada
         print(f' A matriz aumentada após as operações é:')
+        #A matriz aumentada é apresentada arredondada para se facilitar a visualização
         print(np.round(matriz,3))
         
+        #Extração do bloco da direita da matriz aumentada, excluindo-se as colunas da esquerda
         matrizinvertida=matriz
         for contador in range(0,tam):
             matrizinvertida=np.delete(matrizinvertida,0,axis=1)
             
-        
+        #Plotagem da matriz inversa
       
-        print('Extraindo o lado direito da matriz aumentada, temos:')
+        print('Extraindo o lado direito da matriz aumentada, temos a inversa pedida:')
         print(matrizinvertida)
-        print(f'Comparando com a inversa da matriz pela função inv do numpy: ')
-        print(matriz_inversa)
+
+        
+
+#Declaração da matriz da qual se quer tirar a inversa
 matriz=np.array([[2,3,1.4,3,4],[3,2,5,5,2.3],[1,3,2,2,1.2],[2,4.5,6,2,3.4],[1,2,3,4,5]])
 
+#Chamada da função que inverte a matriz
 inversa_eliminacao(matriz)
