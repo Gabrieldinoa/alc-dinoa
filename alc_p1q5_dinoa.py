abef30f9bbdaaf2625d119eede25a30efab89a12
Gabrieldinoa <166359363+Gabrieldinoa@users.noreply.github.com>
2024-05-16 21:15:18 -0300

    Update alc_p1q5_dinoa.py

--- alc_p1q5_dinoa.py
+++ alc_p1q5_dinoa.py
@@ -1,56 +1,54 @@
 import numpy as np
 import math as m
 
-matriz=np.array([[10,1,1,2],[1,3,4,1],[1,2,2,2],[3,4,1,0]])
+matriz=np.array([[2,1,1],[3,3,3],[1,3,2]])
 tam=len(matriz)
-print(tam)
+#print(tam)
+print('a matriz eh:')
 print(matriz)
-
+matriz_inversa=np.linalg.inv(matriz)
 identidade=np.eye(tam)
-print(identidade)
 
+aumentada=np.concatenate((np.transpose(matriz),identidade))
+#print(aumentada)
+#print(identidade)
+aT=np.transpose(aumentada)
+print(aT)
+matriz=aT
 #zerar a primeira coluna
 for k in range(0,tam-1):
-    print(k)
+    
+    
+    matriz[k]=matriz[k]/matriz[k][k]
+    #identidade[k]=identidade[k]/matriz[k][k]
     for n in range(k+1,tam):
-        print(n)
-        matriz[n]=matriz[k][k]*matriz[n]-matriz[n][k]*matriz[k]
-        identidade[n]=matriz[k][k]*identidade[n]-matriz[n][k]*identidade[k]
+        print(f'k eh {k} e n eh {n}')
         
+        
+        matriz[n]=matriz[k][k]*matriz[n]-matriz[n][k]*matriz[k]
+        #identidade[n]=matriz[k][k]*identidade[n]-matriz[n][k]*identidade[k]
+        print(matriz)
 
     
+print(f'matriz superior eh')
 print(matriz)
+
 
 #zerar a parte superior
 
 #zerar a segunda coluna
 for t in range(1,tam):
-    print(t)
+    
     for n in range(0,t):
-        print(t)
+        print(f't eh {t} n eh {n}')
+        
         matriz[n]=matriz[t][t]*matriz[n]-matriz[n][t]*matriz[t]
-        identidade[n]=matriz[t][t]*identidade[n]-matriz[n][t]*identidade[t]
+        #identidade[n]=matriz[t][t]*identidade[n]-matriz[n][t]*identidade[t]
+        print(matriz)
 
+
+
+print('A  matriz final eh')
+matriz=(-1)*matriz
 print(matriz)
-
-# for n in range(0,1):
-#     print(n)
-#     matriz[n]=matriz[1][1]*matriz[n]-matriz[n][1]*matriz[1]
-
-# print(matriz)
-
-# #zerar a terceira coluna
-
-# for n in range(0,2):
-#     print(n)
-#     matriz[n]=matriz[2][2]*matriz[n]-matriz[n][2]*matriz[2]
-
-# print(matriz)
-
-# #zerar a quarta coluna
-
-# for n in range(0,3):
-#     print(n)
-#     matriz[n]=matriz[3][3]*matriz[n]-matriz[n][3]*matriz[3]
-
-# print(matriz)
+print(matriz_inversa)
