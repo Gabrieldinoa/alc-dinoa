6958b095a116ba202626cb36499ddbc33249e9f0
Gabrieldinoa <166359363+Gabrieldinoa@users.noreply.github.com>
2024-05-16 21:54:17 -0300

    Update alc_p1q5_dinoa.py

--- alc_p1q5_dinoa.py
+++ alc_p1q5_dinoa.py
@@ -1,64 +1,49 @@
 import numpy as np
 import math as m
 
-matriz=np.array([[3,1,1,5,7],[3,3,3,1,2],[1,3,2,7,3],[2.4,6,1,8,2],[3,5,3,2,7]])
-tam=len(matriz)
-#print(tam)
-print('A matriz é:')
-print(matriz)
+def inversa_eliminacao(matriz):
 
-avaliador=1
-for cont in range(0,tam):
-    avaliador=matriz[cont][cont]*avaliador
-if avaliador==0:
-    print('Não é possível diagonalização')
-else:
 
-    matriz_inversa=np.linalg.inv(matriz)
-    identidade=np.eye(tam)
-
-    aumentada=np.concatenate((np.transpose(matriz),identidade))
-    #print(aumentada)
-    #print(identidade)
-    aT=np.transpose(aumentada)
     
-    matriz=aT
-    print('A matriz aumentada é:')
+    tam=len(matriz)
+    #print(tam)
+    print('A matriz inicial é:')
     print(matriz)
-    #zerar a primeira coluna
-    for k in range(0,tam-1):
+
+    avaliador=1
+    for cont in range(0,tam):
+        avaliador=matriz[cont][cont]*avaliador
+    if avaliador==0:
+        print('Sugere-se o uso de uma função alternativa para o cálculo da inversa')
+    else:
+        matriz_inversa=np.linalg.inv(matriz)
+        identidade=np.eye(tam)
+        aumentada=np.concatenate((np.transpose(matriz),identidade))
+        aT=np.transpose(aumentada)
+        matriz=aT
+        print('A matriz aumentada é:')
+        print(matriz)
         
-        matriz[k]=matriz[k]/matriz[k][k]
-        #identidade[k]=identidade[k]/matriz[k][k]
-        for n in range(k+1,tam):
-            #print(f'k eh {k} e n eh {n}')
+        for k in range(0,tam-1):
             
+            matriz[k]=matriz[k]/matriz[k][k]
             
-            matriz[n]=matriz[k][k]*matriz[n]-matriz[n][k]*matriz[k]
-            #identidade[n]=matriz[k][k]*identidade[n]-matriz[n][k]*identidade[k]
-            #print(matriz)
-
+            for n in range(k+1,tam):
+                
+                        
+                matriz[n]=matriz[k][k]*matriz[n]-matriz[n][k]*matriz[k]
+                
+            
+        for t in range(1,tam):
+            
+            for n in range(0,t):
+                        
+                matriz[n]=matriz[t][t]*matriz[n]-matriz[n][t]*matriz[t]
+            
+        matriz=(1/matriz[0][0])*matriz
+        print(f' A matriz aumentada invertida é: {matriz}')
+        print(f'Gabarito: {matriz_inversa}')
         
-    #print(f'matriz superior eh')
-    #print(matriz)
+matriz=np.array([[2,1,1,5,7],[3,3,3,1,2],[1,3,2,7,3],[2.4,6,1,8,2],[3,5,3,2,7]])
 
-
-    #zerar a parte superior
-
-    #zerar a segunda coluna
-    for t in range(1,tam):
-        
-        for n in range(0,t):
-            #print(f't eh {t} n eh {n}')
-            
-            matriz[n]=matriz[t][t]*matriz[n]-matriz[n][t]*matriz[t]
-            #identidade[n]=matriz[t][t]*identidade[n]-matriz[n][t]*identidade[t]
-            #print(matriz)
-
-
-
-    matriz=(1/matriz[0][0])*matriz
-    print(f' A matriz aumentada invertida é: {matriz}')
-    print(f'Gabarito: {matriz_inversa}')
-    
-
+inversa_eliminacao(matriz)
